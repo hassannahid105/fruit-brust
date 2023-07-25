53afa39634aa6eadedea9046ed5ceb3b02507f5e
nahidHassan405 <infosharemail405@gmail.com>
2023-07-26 01:55:11 +0600

    team section

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// font-family: 'Roboto', sans-serif;
